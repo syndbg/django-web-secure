e11b2ae685834752e39fced0b3aea62b7085129f
syndg <syndbe@gmail.com>
2014-11-09 22:12:15 +0200

    Added the HSTS, SCP and XSS Filter headers.

--- web_secure/middleware.py
+++ web_secure/middleware.py
@@ -12,3 +12,10 @@ class WebSecureMiddleware:
         self.exempt_hosts = [re.compile(r) for r in settings.SECURE_EXEMPT_HOSTS]
 
         self.hsts_include_subdomains = settings.SECURE_HSTS_INCLUDE_SUBDOMAINS
+        self.hsts_max_age = settings.SECURE_HSTS_MAX_AGE
+        self.hsts_preload = settings.SECURE_HSTS_PRELOAD
+
+        self.content_security_policy = settings.SECURE_CONTENT_POLICY
+        self.content_type_nosniff = settings.SECURE_CONTENT_TYPE_NOSNIFF
+        self.xss_filter = settings.SECURE_XSS_FILTER
+
