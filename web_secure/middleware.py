3553779d3815c5107661fc98be22ad324e0de283
syndg <syndbe@gmail.com>
2014-11-09 22:01:54 +0200

    Added part of security-related HTTP headers

--- web_secure/middleware.py
+++ web_secure/middleware.py
@@ -1,9 +1,14 @@
+import re
+
 from django.conf import settings
+from django.http import HttpResponsePermanentRedirect
 
 
 class WebSecureMiddleware:
 
     def __init__(self):
         self.enforce_ssl = settings.SECURE_ENFORCE_SSL
-        self.hsts_seconds = settings.SECURE_HSTS_SECONDS
+        self.ssl_host = settings.SECURE_SSL_HOST
+        self.exempt_hosts = [re.compile(r) for r in settings.SECURE_EXEMPT_HOSTS]
+
         self.hsts_include_subdomains = settings.SECURE_HSTS_INCLUDE_SUBDOMAINS
