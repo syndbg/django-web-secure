6d79f71b5e146b153e2bcb77cdab15798be72395
syndg <syndbe@gmail.com>
2014-11-10 02:10:23 +0200

    Added the HTTP headers strings as static vars. Also structured the instance fields by order of use

--- web_secure/middleware.py
+++ web_secure/middleware.py
@@ -5,14 +5,18 @@ from django.http import HttpResponsePermanentRedirect
 
 
 class WebSecureMiddleware:
+    CONTENT_SECURITY_POLICY_HEADER = 'Content-Security-Policy'
+    CONTENT_TYPE_OPTIONS_HEADER = 'X-Content-Type-Options'
+    HSTS_HEADER = 'Strict-Transport-Security'
+    XSS_PROTECTION_HEADER = 'X-XSS-Protection'
 
     def __init__(self):
         self.enforce_ssl = settings.SECURE_ENFORCE_SSL
         self.ssl_host = settings.SECURE_SSL_HOST
         self.exempt_hosts = [re.compile(r) for r in settings.SECURE_EXEMPT_HOSTS]
 
-        self.hsts_include_subdomains = settings.SECURE_HSTS_INCLUDE_SUBDOMAINS
         self.hsts_max_age = settings.SECURE_HSTS_MAX_AGE
+        self.hsts_include_subdomains = settings.SECURE_HSTS_INCLUDE_SUBDOMAINS
         self.hsts_preload = settings.SECURE_HSTS_PRELOAD
 
         self.content_security_policy = settings.SECURE_CONTENT_POLICY
